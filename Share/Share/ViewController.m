b631cb02823f4faf95aa2601c35a8d873d1e9beb
CC <meetchenchen@gmail.com>
2016-10-29 15:16:20 +0800

    第二种自定义菜单

--- Share/Share/ViewController.m
+++ Share/Share/ViewController.m
@@ -11,9 +11,11 @@
 #import "CCField.h"
 
 @interface ViewController ()
-{
-    CCField *field;
-}
+
+@property (nonatomic, strong) CCField *field;
+@property (nonatomic, strong) UILabel *label;
+
+
 @end
 
 @implementation ViewController
@@ -21,13 +23,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    field = [[CCField alloc]initWithFrame:CGRectMake(100, 200, 100, 40)];
-    field.backgroundColor = [UIColor greenColor];
-    [self.view addSubview:field];
+    //第一种
+    _field = [[CCField alloc]initWithFrame:CGRectMake(100, 200, 100, 40)];
+    _field.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:_field];
     
     UIMenuItem *shareItem = [[UIMenuItem alloc]initWithTitle:@"分享" action:@selector(click)];
     UIMenuController *menu = [UIMenuController sharedMenuController];
     [menu setMenuItems:[NSArray arrayWithObjects:shareItem, nil]];
+    
+    //第二种
+    _label = [[UILabel alloc]initWithFrame:CGRectMake(100, 400, 100, 100)];
+    _label.backgroundColor = [UIColor redColor];
+    [self.view addSubview:_label];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(click:)];
+    _label.userInteractionEnabled = YES;
+    [_label addGestureRecognizer:tap];
 }
 
 - (void)click {
@@ -37,15 +48,30 @@
     // 发到还有列表 ， 默认是 0
     sendReq.scene = 0;
     
-    sendReq.text = field.text;
+    sendReq.text = _field.text;
     //发送分享信息
     [WXApi sendReq:sendReq];
     NSLog(@"213");
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark -- UIMenuController
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+- (void)click:(UITapGestureRecognizer *)tap {
+    //要显示 menu 必须将触碰的 View 设为第一响应对象
+    [self.view becomeFirstResponder];
+    UIMenuController *menuController = [UIMenuController sharedMenuController];
+    UIMenuItem *copyItem = [[UIMenuItem alloc]initWithTitle:@"复制" action:@selector(click)];
+    menuController.menuItems = @[copyItem];
+    //设置 menu 的 frame和父 view
+    [menuController setTargetRect:CGRectMake(_label.frame.origin.x, _label.frame.origin.y, 100, 50) inView:self.view];
+    [menuController setMenuVisible:YES animated:YES];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [_field resignFirstResponder];
 }
 
 @end
